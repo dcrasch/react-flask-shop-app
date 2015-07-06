3fbd5a7121151103d00862dafc4a8b1fb6cef1a5
David Rasch <david@cloudscripting.nl>
2015-07-06 21:05:52 +0200

    add product variant

--- rfs/products/models.py
+++ rfs/products/models.py
@@ -5,3 +5,15 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(512), nullable=False)
     description = db.Column(db.Text)
+    image = db.Column(db.String(512))
+
+class ProductVariant(db.model)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(512), nullable=False)
+    description = db.Column(db.Text)
+    sku = db.Column(db.String(512))
+    price = db.Column(db.Integer))
+    inventory = db.Column(db.Integer))
+
+    mainproduct_id = db.Column(Integer, ForeignKey('products.id'))
+    mainproduct = relationship("Product", backref=backref('varients', order_by=id))
